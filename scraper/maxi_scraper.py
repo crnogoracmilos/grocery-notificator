5110fa4f5e805d1c6617ba3cc1e58e4fc0202891
crnogoracmilos <crnogoracmilos5@gmail.com>
2026-08-30 23:30:40 +0200

    Fixing the scraper bugs

--- scraper/maxi_scraper.py
+++ scraper/maxi_scraper.py
@@ -4,37 +4,48 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
-driver = webdriver.Chrome()
-url = 'https://cenoteka.rs/p/jaja-klasa-m-10kom/'
-
-try:
-    driver.get(url)
-    time.sleep(3)
-    html_code = driver.page_source
-finally:
-    driver.quit()
-
-soup = BeautifulSoup(html_code, features='html.parser')
-prices_html = soup.find('div', class_=re.compile(r'priceCol'))
-if prices_html:
-    row = prices_html.find_all('div', class_=re.compile(r'row'))
-    d = dict()
-    for prices in row:
-        store = prices.find('img', class_=re.compile(r'logo'))
-        store_name = store['alt'] if store else 'unknown'
-
-        price_span = prices.find('span', class_=re.compile(r'price'))
-        price_text = price_span.text.strip() if price_span else 'unknown'
-
-        if price_span and price_span.text.strip():
-            price_text = price_span.text.strip()
-            price = float(price_text.replace(',', '.'))
-            d[store_name] = price
-    if d:
-        min_price = min(d.values())
-        best_store = " ".join([store for store, price in d.items() if price == min_price])
-        print(best_store)
-else:
-    print("The program was unable to find the prices")
 
 
+
+def parsing():
+    driver = webdriver.Chrome()
+    url = 'https://cenoteka.rs/p/jaja-klasa-m-10kom/'
+    try:
+        driver.get(url)
+        time.sleep(3)
+        html_code = driver.page_source
+    finally:
+        driver.quit()
+    soup = BeautifulSoup(html_code, features='html.parser')
+    prices_html = soup.find('div', class_=re.compile(r'priceCol'))
+    return prices_html
+
+def find_the_least_expensive(prices_html):
+    if prices_html:
+        row = prices_html.find_all('div', class_=re.compile(r'row'))
+        d = dict()
+        for prices in row:
+            store = prices.find('img', class_=re.compile(r'logo'))
+            store_name = store['alt'] if store else 'unknown'
+
+            price_span = prices.find('span', class_=re.compile(r'price'))
+
+            if price_span and price_span.text.strip():
+                price_text = price_span.text.strip() if price_span else 'unknown'
+                price = float(price_text.replace('.', '').replace(',','.'))
+                d[store_name] = price
+            if not d:
+                print("Pronalazenje cena je neuspesno")
+                return None
+        d_sorted = sorted(d.items(), key=lambda item: item[1])
+        best_store, best_price = d_sorted[0]
+        print(f"The best store for buying eggs is: {best_store}, with the price being {best_price}")
+        try:
+            return d_sorted[0]
+        except RuntimeError("Greska, vrednost nije vracena"):
+            return None
+    else:
+        return None
+
+if "__main__" == __name__:
+    print(find_the_least_expensive(parsing()))
