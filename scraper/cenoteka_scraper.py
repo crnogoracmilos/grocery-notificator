ce0953fce2cb6f940c735f7bada96493c3ec7f52
crnogoracmilos <crnogoracmilos5@gmail.com>
2026-09-19 17:29:42 +0200

    Changed the name of the function from parsing to item_getter

--- scraper/cenoteka_scraper.py
+++ scraper/cenoteka_scraper.py
@@ -2,7 +2,7 @@ import re
 from bs4 import BeautifulSoup
 import requests
 
-def parsing(url, timeout = 10):
+def item_getter(url, timeout = 10):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     }
